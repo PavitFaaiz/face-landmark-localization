fb61b041993535c649c782f6dc8fa41fb4e8a221
Pavit <pavit.noin@gmail.com>
2019-01-23 16:41:50 +0700

    Fix naming bug in train.py

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import models
 import keras
 import utils
+import os
+#Set to use only GPU#2
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 weight_load_path = None
 weight_save_path = "weights.h5"
@@ -13,8 +16,8 @@ if __name__ == "__main__":
     # Get data
     train_samples, train_targets, test_samples, test_targets = utils.get_data()
     image_shape = train_samples[0].shape
-    model = models.getModel(image_shape)
-    model.compile(keras.optimizer(lr=lr), loss=model.mse_with_dontcare)
+    model = models.get_model(image_shape)
+    model.compile(optimizer(lr=lr), loss=models.mse_with_dontcare)
     if weight_load_path is not None:
         model.load_weights(weight_load_path)
     #Begin training
